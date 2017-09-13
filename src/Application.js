f739d8c54ccd1ac6ea431f2da04cae3eca038a82
arapl3y <rapley3@gmail.com>
2017-09-13 14:35:18 +1000

    Implement basic Google OAuth

--- src/Application.js
+++ src/Application.js
@@ -1,14 +1,24 @@
-import React, { Component } from 'react';
-import { auth, database } from './firebase';
-import CurrentUser from './CurrentUser';
-import SignIn from './SignIn';
-import NewRestaurant from './NewRestaurant';
-import Restaurants from './Restaurants';
-import './Application.css';
+import React, { Component } from "react";
+import { auth, database } from "./firebase";
+import CurrentUser from "./CurrentUser";
+import SignIn from "./SignIn";
+import NewRestaurant from "./NewRestaurant";
+import Restaurants from "./Restaurants";
+import "./Application.css";
 
 class Application extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      currentUser: null
+    };
+  }
+
+  componentDidMount() {
+    auth.onAuthStateChanged(currentUser => {
+      console.log("AUTH_CHANGE", currentUser);
+      this.setState({ currentUser });
+    });
   }
 
   render() {
@@ -17,6 +27,9 @@ class Application extends Component {
         <header className="Application--header">
           <h1>Lunch Rush</h1>
         </header>
+        <div>
+          <SignIn />
+        </div>
       </div>
     );
   }
--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,12 @@
-import firebase from 'firebase';
+import firebase from "firebase";
 
-const config = {
-  apiKey: '',
-  authDomain: '',
-  databaseURL: '',
-  storageBucket: '',
-  messagingSenderId: ''
+var config = {
+  apiKey: "AIzaSyB9RZk0nqCHGD25mM0lE_fV6Ob5vziAVzw",
+  authDomain: "lunch-rush-bf865.firebaseapp.com",
+  databaseURL: "https://lunch-rush-bf865.firebaseio.com",
+  projectId: "lunch-rush-bf865",
+  storageBucket: "lunch-rush-bf865.appspot.com",
+  messagingSenderId: "997571665882"
 };
 
 firebase.initializeApp(config);
